a9e82e548ca8d919d613f64c3f449486cbec3822
serpient <littlegangwolf@gmail.com>
2018-09-13 16:19:21 -0700

    create svgs

--- src/App.js
+++ src/App.js
@@ -26,21 +26,40 @@ class App extends Component {
   }
   
   appendNewNodes = () => {
-    var dataset = [5, 10, 15, 20, 25, 36, 72, 100];
-    d3.select("body").selectAll("p")
+    var dataset = [100, 7, 5, 26, 11, 8, 25, 14, 23, 19,
+      14, 11, 22, 29, 11, 13, 12, 17, 18, 10,
+      24, 18, 25, 9, 3];
+    d3.select("body").selectAll("div")
       .data(dataset)
       .enter()
-      .append("p")
-      .text((d) => { return d })
+      .append("div")
+      .attr("class", "bar")
+      .style("height", (d) => { return (d*2) + "px"; });
   }
+  createSVGs = () => {
+    var svg = d3.select("body")
+                .append("svg")
+                .attr("width", 500)
+                .attr("height", 50);
+
+    var dataset = [5, 10, 15, 20, 25];
+
+    var circles = svg.selectAll("circle")
+                    .data(dataset)
+                    .enter()
+                    .append("circle");
+    circles.attr("cx", (d, i) => { return (i * 100) + 25})
+            .attr("cy", (d) => { return d / 2 })
+            .attr("r", (d) => { return d })
+            .attr("fill", "yellow")
+            .attr("stroke", "orange")
+            .attr("stroke-width", (d) => { return d / 2 })
+  }
+  
   render() {
     return (
-      <div className="App" onClick={() => this.appendNewNodes()}>
-        <p>Tester 0</p>
-        <p>Tester 1</p>
-        <p>Tester 2</p>
-        <p>Tester 3</p>
-        <p>Tester 4</p>
+      <div className="App" onClick={() => this.createSVGs()}>
+        <p>Click Me</p>
       </div>
     );
   }
